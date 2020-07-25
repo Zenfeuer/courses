9047ac6faffc522facc4799453e5768ce319b25b
Darwing Hernandez <darwinghga@gmail.com>
2020-07-24 20:20:14 -0500

    Update maxPairwiseProduct.py

--- Coursera/DSAS/AT/maxPairwiseProduct.py
+++ Coursera/DSAS/AT/maxPairwiseProduct.py
@@ -22,42 +22,34 @@
 n = int(input())
 a = [int(x) for x in input().split()]
 
-# list: array of numbers (unsorted)
-# isAsc: desired order. True: ascending | False: descending
-def bubbleSort(list, isAsc):
+maxOne = -1
+maxTwo = -1
+i = 0
 
-    # saves the length of the list.
-    n = len(list)
+while i < n:
 
-    # for each iteration, greatest/smallest element must be moved to n-i position in the array, this
-    # ensures that the array it is ordered by the specified order (ascending or descending).
-    for i in range(1, n):
+    if maxOne < a[i]:
+        
+        if maxOne > maxTwo:
+            maxTwo = maxOne
 
-        # j-th and (j+1)-th element must be compared to evaluate if they need to be swapped 
-        # according to the desired order.
-        for j in range(0, n - i):
+        maxOne = a[i]
 
-            # isAsc and list[j] > list[j+1]:
-            # if the desired order is ascending and the j-th element is greater than the (j+1)-th 
-            # element, then a swap must be made.
-            #
-            # not isAsc and list[j] < list[j+1]:
-            # if the desired order is descending and the j-th element is lesser than the (j+1)-th
-            # element, then a swap must be made.
-            if (isAsc and list[j] > list[j+1]) or (not isAsc and list[j] < list[j+1]):
+    elif maxTwo < a[i]:
 
-                # Swapping j-th and (j+1)-th elements
-                tmp = list[j]
-                list[j] = list[j+1]
-                list[j+1] = tmp
+        if maxTwo > maxOne:
+            maxOne = maxTwo
 
-    # returns the sorted list
-    return list
+        maxTwo = a[i]
+    
+    if (i+1) < n and maxTwo < a[i+1]:
 
-assert(len(a) == n)
+        if maxTwo > maxOne:
+            maxOne = maxTwo
 
-# order the array of elements so to get the result is enough to multiply a[0] and a[1].
-bubbleSort(a, False)
+        maxTwo = a[i+1]
+        
+    i += 2
 
 # printing the result
-print(a[0] * a[1])
+print(maxOne * maxTwo)
