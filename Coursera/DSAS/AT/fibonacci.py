c3f8e4f608c509cd1189f4dbc8ba5381ca5561f6
Darwing Hernandez <darwing.her69@gmail.com>
2020-07-25 13:48:11 -0500

    Update fibonacci.py
    Improvements

--- Coursera/DSAS/AT/fibonacci.py
+++ Coursera/DSAS/AT/fibonacci.py
@@ -29,12 +29,15 @@ def fibonacciFast(n):
     f0 = 0
     f1 = 1
     fn = 0
+    i = 2
 
-    for i in range(2, n+1):
+    while i <= n:
 
         fn = f0 + f1
         f0 = f1
         f1 = fn
+
+        i += 1
 
     return fn
 
