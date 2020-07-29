02537c5ed5cb4b8d357731ee61d2cb06bcc13483
Darwing Hernandez <darwing.her69@gmail.com>
2020-07-29 17:30:53 -0500

    Create gcdNumbers.py

---
+++ Misc/gcdNumbers.py
@@ -0,0 +1,18 @@
+def gcd(a, b):
+
+    if b == 0:
+        return a
+
+    return gcd(b, a%b)
+
+def gcdNumbers(num, arr):
+
+    for i in range(num - 1):
+        arr[i+1] = gcd(arr[i], arr[i+1])
+
+    return arr[num-1]
+
+num = int(input())
+arr = [int(x) for x in input().split()]
+
+print(gcdNumbers(num, arr))
