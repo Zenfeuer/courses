45740b7ba5a7a6c0dc12bbaf37cf87b60f2f8a0f
Darwing Hernandez <darwing.her69@gmail.com>
2020-07-29 20:39:42 -0500

    Improved GCD and added last digit large fibonacci number

--- Coursera/DSAS/AT/gcd.py
+++ Coursera/DSAS/AT/gcd.py
@@ -1,5 +1,17 @@
 #!/bin/python3
 
+# @author: Darwing Hernandez <darwinghga@gmail.com>
+
+# Greatest Common Divisor
+#
+# The greatest common divisor GCD(a, b) of two non-negative integers a and b (which are not both 
+# equal to 0) is the greatest integer d that divides both a and b.
+# 
+# GCD(a, b) = GCD(b, a'), where a' is the remainder of a/b (Euclidean algorithm)
+#
+# Input:   integers a and b; 1 <= a,b <= 2*10^9
+# Output:  GCD(a, b)
+
 def gcd(a, b):
 
     if b == 0:
@@ -7,5 +19,6 @@ def gcd(a, b):
     
     return gcd(b, a%b)
 
+params = [int(x) for x in input().split()]
 
-print(gcd(3, 99))
+print(gcd(params[0], params[1]))
---
+++ Coursera/DSAS/AT/lastDigitLargeFib.py
@@ -0,0 +1,75 @@
+#!/bin/python3
+
+# @author: Darwing Hernandez <darwinghga@gmail.com>
+
+# Last Digit of a Large Fibonacci Number
+#
+# To get the last digit for a large fibonacci number, the fast approach is to carry the last digit
+# using mod 10
+# 
+# F(0) = 0
+# F(1) = 1
+# F(n) = (F(n-1) + F(n-2)) mod 10
+#
+# Input:   integer n; 0 <= n <= 10^7
+# Output:  Fib(n) mod 10
+
+import random
+
+n = int(input())
+
+# Recursive version that is slow
+def lastDigitFibNaive(n):
+
+    if n <= 1:
+        return n
+    else:
+        return (lastDigitFibNaive(n-1) + lastDigitFibNaive(n-2)) % 10
+
+# Iterative version that is faster
+def lastDigitFibFast(n):
+
+    if n <= 1:
+        return n
+
+    f0 = 0
+    f1 = 1
+    fn = 0
+    i = 2
+
+    while i <= n:
+
+        fn = (f0 + f1) % 10
+        f0 = f1
+        f1 = fn
+
+        i += 1
+
+    return fn
+
+# Stress testing both solutions
+def stressTest(n):
+
+    while True:
+
+        randomSize = random.randint(0, n)
+
+        print(randomSize)
+
+        result1 = lastDigitFibNaive(randomSize)
+        result2 = lastDigitFibFast(randomSize)
+
+        if result1 != result2:
+            print("Wrong answer: ", result1, result2)
+            break
+        else:
+            print("OK")
+
+# Stress testing
+stressTest(n)
+
+# Call fast solution
+#result = fibonacciFast(n)
+
+# Print result
+#print(result)
